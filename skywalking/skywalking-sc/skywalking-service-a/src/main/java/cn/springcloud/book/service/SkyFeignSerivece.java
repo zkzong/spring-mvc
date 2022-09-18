f946b0de191f72e3dbecd669e4e2629a8cdbd8eb
zkzong <zongzhankui@hotmail.com>
2022-09-18 12:46:41 +0800

    skywalking

---
+++ skywalking/skywalking-sc/skywalking-service-a/src/main/java/cn/springcloud/book/service/SkyFeignSerivece.java
@@ -0,0 +1,19 @@
+package cn.springcloud.book.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "skywalking-service-b")
+public interface SkyFeignSerivece {
+
+    /**
+     * 调用service-b
+     *
+     * @param serviceName 传递过去的是service-a
+     * @return
+     */
+    @RequestMapping(value = "/getSendInfo", method = RequestMethod.GET)
+    String getSendInfo(@RequestParam("serviceName") String serviceName);
+}
